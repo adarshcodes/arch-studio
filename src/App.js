b0984c4a3688333ff695cc349d1307ea627d1549
adarshcodes <adarsh.lrnr@gmail.com>
2022-05-30 16:58:11 +0530

    adding all the routes to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+// Importing Routes
 import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+import Portfolio from "./routes/portfolio/portfolio.component";
+import AboutUs from "./routes/AboutUs/about-us.component";
+import Contact from "./routes/contact/contact.component";
 
 function App() {
 	return (
 		<main className="app">
-			<Navigation />
+			<Routes>
+				<Route path="/" element={<Navigation />}>
+					<Route index element={<Home />} />
+					<Route path="/portfolio" element={<Portfolio />} />
+					<Route path="/about-us" element={<AboutUs />} />
+					<Route path="/contact" element={<Contact />} />
+				</Route>
+			</Routes>
 		</main>
 	);
 }
