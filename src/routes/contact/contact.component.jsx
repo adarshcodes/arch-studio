8161f12cdd3df8e537a079e795b7a068cab74557
adarshcodes <adarsh.lrnr@gmail.com>
2022-06-06 17:27:49 +0530

    completed contact hero

--- src/routes/contact/contact.component.jsx
+++ src/routes/contact/contact.component.jsx
@@ -3,6 +3,23 @@ import Helmet from "react-helmet";
 
 // importing components
 import Pagination from "../../components/pagination.component";
+import AboutHero from "../../components/aboutHero.component";
+
+// importing images
+import hero from "../../assets/images/contact/image-hero.jpg";
+import heroMob from "../../assets/images/contact/image-hero-mob.jpg";
+import heroTab from "../../assets/images/contact/image-hero-tab.jpg";
+
+const heroData = {
+	image: {
+		mobile: heroMob,
+		tablet: heroTab,
+		desktop: hero,
+	},
+	title: "Contact",
+	heading: "Tell us about your project",
+	desc: "We’d love to hear more about your project. Please, leave a message below or give us a call. We have two offices, one in Texas and one in Tennessee. If you find yourself nearby, come say hello!",
+};
 
 export default function Contact() {
 	useEffect(() => {
@@ -15,6 +32,12 @@ export default function Contact() {
 				<title>Arch Studio | Contact Us</title>
 			</Helmet>
 			<Pagination page={"contact"} />
+			<AboutHero
+				image={heroData.image}
+				title={heroData.title}
+				heading={heroData.heading}
+				desc={heroData.desc}
+			/>
 		</div>
 	);
 }
