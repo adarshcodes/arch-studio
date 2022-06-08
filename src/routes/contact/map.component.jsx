f8547e7c4b79b31a32f53aa04abb08bda643c97b
adarshcodes <adarsh.lrnr@gmail.com>
2022-06-08 21:26:36 +0530

    removing the scroll on map

--- src/routes/contact/map.component.jsx
+++ src/routes/contact/map.component.jsx
@@ -4,7 +4,7 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 function Map() {
 	return (
 		<div className="map-container">
-			<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+			<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
