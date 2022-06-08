95c27d9cff82171c6c59861669b51b4fbeeb98a2
adarshcodes <adarsh.lrnr@gmail.com>
2022-06-08 21:21:01 +0530

    adding map to the contact page

--- src/routes/contact/map.component.jsx
+++ src/routes/contact/map.component.jsx
@@ -9,7 +9,7 @@ function Map() {
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
-				<Marker position={[51.505, -0.09]}>
+				<Marker position={[51.54, -0.09]}>
 					<Popup>3399 Wines Lane TX</Popup>
 				</Marker>
 				<Marker position={[51.5, -0.2]}>
