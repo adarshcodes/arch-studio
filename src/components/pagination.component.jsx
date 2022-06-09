1b817d29e55e485b7734779cb37e6e1a09d98f79
adarshcodes <adarsh.lrnr@gmail.com>
2022-06-09 10:15:46 +0530

    fixing pagination floating issue

--- src/components/pagination.component.jsx
+++ src/components/pagination.component.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 export default function Pagination(props) {
 	return (
-		<div className="pagename-animator">
-			<div className="pagename">
-				<div className="pagename__line"></div>
-				<p className="pagename__page">{props.page}</p>
-			</div>
+		<div className="pagename">
+			<span className="pagename__line"></span>
+			<p className="pagename__page">{props.page}</p>
 		</div>
 	);
 }
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 // importing components
-import Pagination from "../../components/pagination.component";
 import Hero from "./hero.component";
 import Welcome from "./welcome.component";
 import SmallTeams from "./smallTeams.component";
 import Featured from "./featured-component";
+import Pagination from "../../components/pagination.component";
 
 export default function Home() {
 	return (
