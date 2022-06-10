c3d28f3fc0a745ce56fba733fb69736e6cca4559
adarshcodes <adarsh.lrnr@gmail.com>
2022-06-10 10:43:52 +0530

    adding ham-menu to the navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,8 +2,16 @@ import React, { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/images/logo.svg";
+import ham from "../../assets/images/icons/icon-hamburger.svg";
+import close from "../../assets/images/icons/icon-close.svg";
 
 export default function Navigation() {
+	const [hamVisible, setHamVisible] = React.useState(false);
+
+	function hamshow() {
+		setHamVisible(!hamVisible);
+	}
+
 	return (
 		<Fragment>
 			<nav className="navbar">
@@ -21,6 +29,27 @@ export default function Navigation() {
 						<Link to="/contact">Contact</Link>
 					</li>
 				</ul>
+
+				<img
+					src={!hamVisible ? ham : close}
+					alt="hamburger"
+					className="ham"
+					onClick={hamshow}
+				/>
+
+				<div className={`ham-nav ${hamVisible ? "toggle-ham" : null}`}>
+					<ul className="navbar__list ham__list">
+						<li className="navbar__list-item">
+							<Link to="/portfolio">Portfolio</Link>
+						</li>
+						<li className="navbar__list-item">
+							<Link to="/about-us">About Us</Link>
+						</li>
+						<li className="navbar__list-item">
+							<Link to="/contact">Contact</Link>
+						</li>
+					</ul>
+				</div>
 			</nav>
 			<Outlet />
 		</Fragment>
